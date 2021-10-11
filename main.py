66981afa33a34cc391e8deb959a2ca56c47a74f8
AlejandroMagdaleno <alxmg179@gmail.com>
2021-10-10 23:37:07 -0400

    HW3

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import requests
+import scipy as scipy
+import spacy
+import nltk
+from nltk.tokenize import sent_tokenize
 from bs4 import BeautifulSoup
+import fasttext
+import numpy as np
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
 
 
 def get_article(url):
@@ -12,8 +21,44 @@ def get_article(url):
         f.write(body)
 
 
+def cos_sim(a, b):
+    dot_product = np.dot(a, b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a * norm_b)
+
+
 if __name__ == '__main__':
-    # make list of urls, iterate through each url calling get_article[x] and writing to file a new file each time with different name
-    get_article('https://transcripts.cnn.com/show/cnr/date/2021-10-03/segment/06')
+    # get_article('url of cnn article')
+    nlp = spacy.load("en_core_web_sm")
+
+    out = []
+    seen = set()
+
+    with open('news.txt') as f:
+        lines = f.read()
+        doc = nlp(lines)
+
+        num_of_sentences = sent_tokenize(lines)
+
+        for token in doc:
+            if token.is_punct is False and token not in seen:
+                out.append(token.text)
+            seen.add(token.text)
+
+    model = fasttext.train_unsupervised('news.txt', model='skipgram')
+
+    print(len(num_of_sentences))
+    print(num_of_sentences[2])
 
 
+    print(model['welcome'])
+    print(cos_sim(model['spending'], model['america']))
+
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit([lines])
+
+    cv = CountVectorizer(ngram_range=(5, 5), stop_words='english')
+    X = cv.fit_transform(lines)
+    Xc = (X.T * X)
+    Xc.setdiag(0)
